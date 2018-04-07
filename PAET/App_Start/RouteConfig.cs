2f002a6f3996fa822ad5459adbaf45892e783f45
Julio Gomez <rdemantua@hotmail.es>
2018-04-07 13:47:32 +0200

    Prototipo 1

--- PAET/App_Start/RouteConfig.cs
+++ PAET/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PAET
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Sigin", id = UrlParameter.Optional }
             );
         }
     }
---
+++ PAET/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PAET.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Sigin()
+        {
+            Session.Clear();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ComprobarAcceso(FormCollection form)
+        {
+            return View();
+        }
+    }
+}
